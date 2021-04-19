42beb8a288692229db584ece063bdeab24e39709
Guilherme Ehrat Zils <guilhermezils@icloud.com>
2021-04-19 18:26:31 +0000

    Automatically backed up by Learn

---
+++ example_file.rb
@@ -0,0 +1,7 @@
+array = [100, 300, 50, 450]
+count = 0
+ 
+while count < array.length do
+  puts array.sort![count]
+  count += 1
+end
---
+++ learning-comments.rb
@@ -0,0 +1,5 @@
+def my_ruby_method
+  puts local_variable
+end
+
+my_ruby_method
